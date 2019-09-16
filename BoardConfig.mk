e560538536bacb19066ed9a81df79bdd1e1f2543
Nebrassy <nebras30@gmail.com>
2019-09-16 13:49:54 +0300

    enchilada: Include prebuilt TWRP
    Change-Id: I22bdd53f6e9a3c61f92162928be5185a2438bdc1
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,5 +22,8 @@ DEVICE_PATH := device/oneplus/enchilada
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 
+# Prebuilt TWRP include
+-include device/twrp/enchilada/twrp.mk
+
 # inherit from the proprietary version
 -include vendor/oneplus/enchilada/BoardConfigVendor.mk
