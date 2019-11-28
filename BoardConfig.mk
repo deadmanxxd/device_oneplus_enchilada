e3291ce39dc17e9ce9fad9cf154be4f3f6f67cca
Bauuuuu <abansode@outlook.com>
2019-11-28 10:02:44 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,6 +19,8 @@
 
 DEVICE_PATH := device/oneplus/enchilada
 
+TARGET_OTA_ASSERT_DEVICE := enchilada,OnePlus6,oneplus6
+
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 
